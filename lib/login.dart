1c73008fe915b649c7427f0c94e3bc300a329496
divya-k-nair <divyakaladharannair@gmail.com>
2021-07-06 12:55:27 +0530

    project completed

---
+++ lib/login.dart
@@ -0,0 +1,116 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+class Login extends StatelessWidget {
+  TextEditingController pname=TextEditingController();
+  TextEditingController eid=TextEditingController();
+  TextEditingController mob=TextEditingController();
+  TextEditingController doc=TextEditingController();
+  TextEditingController sym=TextEditingController();
+  @override
+  Widget build(BuildContext context) {
+    return SingleChildScrollView(
+      child: Container(
+        margin: EdgeInsets.all(20),
+        child: Column(
+          children: [
+            SizedBox(height: 20,),
+            TextField(
+              controller: pname,
+             decoration: InputDecoration(
+               prefix: CircularProgressIndicator(),
+               prefixIcon: Icon(Icons.account_circle,color: Colors.deepOrange,size: 40,),
+               hintText: "Patient name",
+               labelText: "Patient name",
+               border: OutlineInputBorder(
+                 borderRadius: BorderRadius.circular(20),
+               ),
+             ),
+            ),
+            SizedBox(height: 20,),
+            TextField(
+              controller: eid,
+              decoration: InputDecoration(
+                  hintText: "Email id:",
+                    labelText: "Email id:",
+                    prefix: CircularProgressIndicator(),
+                    prefixIcon: Icon(Icons.email,color: Colors.purpleAccent,size: 40,),
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(20),
+                    ),
+              ),
+            ),
+            SizedBox(height: 20,),
+            TextField(
+              controller: mob,
+              decoration: InputDecoration(
+                prefix: CircularProgressIndicator(),
+                prefixIcon: Icon(Icons.add_call,color: Colors.indigo,size: 40,),
+                hintText: "Mobile no:",
+                labelText: "Mobile no:",
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(20),
+                ),
+              ),
+            ),
+            SizedBox(height: 20,),
+            TextField(
+              controller: doc,
+              decoration:InputDecoration(
+                prefix:CircularProgressIndicator(),
+                prefixIcon: Icon(Icons.account_circle_sharp,color: Colors.lightBlueAccent,size: 40,),
+                hintText: "Doctor",
+                labelText: "Doctor",
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(20),
+                ),
+              ) ,
+            ),
+            SizedBox(height: 20,),
+            TextField(
+              controller: sym,
+              decoration: InputDecoration(
+                hintText: "Symptoms",
+                labelText: "Symptoms",
+                prefix: CircularProgressIndicator(),
+                prefixIcon: Icon(Icons.arrow_forward_ios,color: Colors.purpleAccent,size: 40,),
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(20),
+                ),
+              ),
+            ),
+            SizedBox(height: 20,),
+        SizedBox(height: 20,),
+            SizedBox(
+              height: 60,
+              width: double.infinity,
+              child: RaisedButton(
+                color: Colors.pinkAccent,
+                onPressed: (){
+                  var getPatientname=pname.text;
+                  var getEmailid=eid.text;
+                  var getMobile=mob.text;
+                  var getDoctor=doc.text;
+                  var getSymptoms=sym.text;
+                  print(getPatientname);
+                  print(getEmailid);
+                  print(getMobile);
+                  print(getDoctor);
+                  print(getSymptoms);
+
+                
+                
+              },
+                child: Text("SUBMIT",
+                  style: TextStyle(
+                    fontSize: 25,
+                  ),
+              ),
+            ),
+            ),
+           ],//children
+         ),
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
-
-
+import 'package:covid_app/login.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 
@@ -7,129 +6,19 @@ void main()
 {
   runApp(App());
 }
-class App extends StatelessWidget {
-  TextEditingController pname=TextEditingController();
-  TextEditingController email=TextEditingController();
-  TextEditingController mob=TextEditingController();
-  TextEditingController dctr=TextEditingController();
-  TextEditingController sym=TextEditingController();
+class App extends StatelessWidget{
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.white,
+        backgroundColor: Colors.lime,
         appBar: AppBar(
           title: Text("COVID APP"),
         ),
-        body:
-        SingleChildScrollView(
-          child: Container(
-            margin: EdgeInsets.all(20),
-            child: Column(
-              children: [
-                SizedBox(height: 10,),
-                TextField(
-                  controller: pname,
-                  decoration: InputDecoration(
-                    prefixIcon: Icon(Icons.account_circle,color: Colors.red,size: 40,),
-                    prefix: CircularProgressIndicator(),
-                    labelText: "Patient name",
-                    hintText: "Patient name",
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(20),
-                    )
-                  ),
-                ),
-                SizedBox(height: 10,),
-                TextField(
-                  controller: email,
-                  decoration: InputDecoration(
-                    prefixIcon: Icon(Icons.email,color: Colors.deepOrange,size: 40,),
-                    prefix: CircularProgressIndicator(),
-                    labelText: "Email id",
-                    hintText: "Email id:",
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(20),
-                    )
-                  ),
-                ),
-                SizedBox(height: 10,),
-                TextField(
-                  controller: mob,
-                  decoration: InputDecoration(
-                    prefixIcon: Icon(Icons.add_call,color: Colors.lime,size: 40,),
-                    prefix: CircularProgressIndicator(),
-                    labelText: "Mobile no:",
-                    hintText: "Mobile no:",
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(20),
-                    )
-                  ),
-                ),
-                SizedBox(height: 10,),
-                TextField(
-                  controller: dctr,
-                  decoration: InputDecoration(
-                    prefixIcon: Icon(Icons.account_circle,color: Colors.teal,size: 40,),
-                    prefix: CircularProgressIndicator(),
-                    labelText: "Doctor",
-                    hintText: "Doctor",
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(20),
-                    )
-                  ),
-                ),
-                SizedBox(height: 10,),
-                TextField(
-                  maxLines: 2,
-                  controller: sym,
-                  decoration: InputDecoration(
-                    prefixIcon: Icon(Icons.arrow_forward,color: Colors.greenAccent,size: 40,),
-                    prefix: CircularProgressIndicator(),
-                    labelText: "Symptoms",
-                    hintText: "Symptoms",
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(20),
-                    )
-                  ),
-                ),
-                SizedBox(height: 10,),
-                SizedBox(
-                  height: 60,
-                  width: double.infinity,
-                  child: RaisedButton(
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(20),
-                    ),
-                    color: Colors.purpleAccent,
-                    onPressed: (){
-                      var getPatientname=pname.text;
-                      var getEmailid=email.text;
-                      var getMobileno=int.parse(mob.text);
-                      var getDoctor=dctr.text;
-                      var getSymptoms=sym.text;
-                      print(getPatientname);
-                      print(getEmailid);
-                      print(getMobileno);
-                      print(getDoctor);
-                      print(getSymptoms);
-
-                    },
-                    child: Text("SUBMIT",
-                      style: TextStyle(
-                        color: Colors.black,
-                        fontSize: 25,
-                      ),
-                    ),
-
-                  ),
-                ),
-              ],//children
-            ),
-          ),
-        ),
+        body:Login(),
       ),
+
     );
   }
 }
